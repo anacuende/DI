e71f7c2116ee013c88bd6a6d9264ad356e11f92a
anacuende <acuendet@fpcoruna.afundacion.org>
2023-09-25 12:34:47 +0200

    Ejercicio 2

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,3 +1,4 @@
+puntos = 0
 print("Tengo agujas pero no sé coser, tengo números pero no sé leer, las horas te doy, ¿Sabes quién soy?")
 print("a) Mesa")
 print("b) Reloj")
@@ -5,5 +6,30 @@ print("c) Libro")
 respuesta = input("Teclee respuesta (a/b/c): ")
 if respuesta != "b":
 	print("Incorrecto, la respuesta correcta es B")
+	puntos = puntos - 5
 else:
 	print ("Correcto")
+	puntos = puntos + 10
+print("Blanco por dentro, verde por fuera, si quieres saberlo, espera. ¿Qué es?")
+print("a) Pera")
+print("b) Tomate")
+print("c) Uva")
+respuesta = input("Teclee respuesta (a/b/c): ")
+if respuesta != "a":
+	print("Incorrecto, la respuesta correcta es A")
+	puntos = puntos - 5
+else:
+	print("Correcto")
+	puntos = puntos + 10
+print("Tiene llaves pero no abre puertas, tiene espacio pero no tiene habitantes. ¿Qué es?")
+print("a) Televisión")
+print("b) Silla")
+print("c) Teclado")
+respuesta = input("Teclee respuesta (a/b/c): ")
+if respuesta != "c":
+	print("Incorrecto, la respuesta es C")
+	puntos = puntos - 5
+else:
+	print("Correcto")
+	puntos = puntos + 10
+print("Tu puntuación final es: ", puntos)
