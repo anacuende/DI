9e281273ac7187e0a5c26059bf761b4a533bfbf4
anacuende <acuendet@fpcoruna.afundacion.org>
2023-09-25 12:09:02 +0200

    Ejercicio 1

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,9 @@
+print("Tengo agujas pero no sé coser, tengo números pero no sé leer, las horas te doy, ¿Sabes quién soy?")
+print("a) Mesa")
+print("b) Reloj")
+print("c) Libro")
+respuesta = input("Teclee respuesta (a/b/c): ")
+if respuesta != "b":
+	print("Incorrecto, la respuesta correcta es B")
+else:
+	print ("Correcto")
