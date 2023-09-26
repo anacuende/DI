771cef94c960284859ba996568fdfd7e2e340b1c
anacuende <acuendet@fpcoruna.afundacion.org>
2023-09-26 10:20:49 +0200

    Ejercicio 6

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,35 +1,48 @@
+import random
+
 puntos = 0
-print("Tengo agujas pero no sé coser, tengo números pero no sé leer, las horas te doy, ¿Sabes quién soy?")
-print("a) Mesa")
-print("b) Reloj")
-print("c) Libro")
-respuesta = input("Teclee respuesta (a/b/c): ")
-if respuesta != "b":
-	print("Incorrecto, la respuesta correcta es B")
-	puntos = puntos - 5
-else:
-	print ("Correcto")
-	puntos = puntos + 10
-print("Blanco por dentro, verde por fuera, si quieres saberlo, espera. ¿Qué es?")
-print("a) Pera")
-print("b) Tomate")
-print("c) Uva")
-respuesta = input("Teclee respuesta (a/b/c): ")
-if respuesta != "a":
-	print("Incorrecto, la respuesta correcta es A")
-	puntos = puntos - 5
-else:
-	print("Correcto")
-	puntos = puntos + 10
-print("Tiene llaves pero no abre puertas, tiene espacio pero no tiene habitantes. ¿Qué es?")
-print("a) Televisión")
-print("b) Silla")
-print("c) Teclado")
-respuesta = input("Teclee respuesta (a/b/c): ")
-if respuesta != "c":
-	print("Incorrecto, la respuesta es C")
-	puntos = puntos - 5
-else:
-	print("Correcto")
-	puntos = puntos + 10
-print("Tu puntuación final es: ", puntos)
+
+adivinanzas = [
+	{
+		"adivinanza": "Tengo agujas pero no sé coser, tengo números pero no sé leer, las horas te doy, ¿Sabes quién soy?",
+		"opciones": {
+			"a": "Mesa",
+			"b": "Reloj",
+			"c": "Libro"
+		},
+		"respuesta_correcta": "b"
+	},
+	{
+		"adivinanza": "Blanco por dentro, verde por fuera, si quieres saberlo, espera. ¿Qué es?",
+		"opciones": {
+			"a": "Pera",
+			"b": "Tomate",
+			"c": "Uva"
+		},
+		"respuesta_correcta": "a"
+	},
+	{
+		"adivinanza": "Tiene llaves pero no abre puertas, tiene espacio pero no tiene habitantes. ¿Qué es?",
+		"opciones": {
+			"a": "Televisión",
+			"b": "Silla",
+			"c": "Teclado"
+		},
+		"respuesta_correcta": "c"
+	}
+]
+
+selec_adivinanza = random.sample(adivinanzas, 2)
+
+for adivinanza in selec_adivinanza:
+	print(adivinanza["adivinanza"])
+	for opcion, respuesta in adivinanza["opciones"].items():
+		print(f"{opcion}) {respuesta}")
+	respuesta = input("Teclee respuesta (a/b/c): ")
+	if respuesta == adivinanza["respuesta_correcta"]:
+		print("Correcto")
+		puntos += 10
+	else:
+		print(f"Incorrecto, la respuesta es {adivinanza['respuesta_correcta']}")
+		puntos -= 5
+print("Puntuacion: ", puntos)
