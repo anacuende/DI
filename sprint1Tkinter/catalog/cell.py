6f74b70766488f8b7006c7cba6dfd09b751bf1d8
anacuende <acuendet@fpcoruna.afundacion.org>
2023-10-06 11:01:03 +0200

    Ejercicio 6

--- sprint1Tkinter/catalog/cell.py
+++ sprint1Tkinter/catalog/cell.py
@@ -4,4 +4,9 @@ class Cell:
     def __init__(self, title, path):
         self.title = title
         self.path = path
-        self.image_tk = ImageTk.PhotoImage(file=path)
+        self.load_and_resize_image()
+
+    def load_and_resize_image(self):
+        original_image = Image.open(self.path)
+        resized_image = original_image.resize((100, 100), Image.LANCZOS)
+        self.image_tk = ImageTk.PhotoImage(resized_image)
--- sprint1Tkinter/catalog/window.py
+++ sprint1Tkinter/catalog/window.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from cell import Cell
+from PIL import Image, ImageTk
 
 class Window:
     def on_button_clicked(self, cell):
@@ -12,14 +13,18 @@ class Window:
         root.title("Ventana")
 
         self.cells = [
-            Cell("Tomb Raider Anniversary", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\edited\\imagen1.png"),
-            Cell("Crash Bandicoot: The Wrath of Cortex", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\edited\\imagen2.png"),
-            Cell("Assassins Creed Syndicate", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\edited\\imagen3.png"),
-            Cell("Detroit Become Human", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\edited\\imagen4.png"),
-            Cell("Marvel Spider-Man", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\edited\\imagen5.png")
+            Cell("Tomb Raider Anniversary", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\unedited\\imagen1.png"),
+            Cell("Crash Bandicoot: The Wrath of Cortex", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\unedited\\imagen2.png"),
+            Cell("Assassins Creed Syndicate", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\unedited\\imagen3.png"),
+            Cell("Detroit Become Human", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\unedited\\imagen4.png"),
+            Cell("Marvel Spider-Man", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\unedited\\imagen5.png")
         ]
 
         for i, cell in enumerate(self.cells):
+            img = Image.open(cell.path)
+            image = img.resize((100, 100), Image.Resampling.LANCZOS)
+            cell.image_tk = ImageTk.PhotoImage(image)
+            
             label = ttk.Label(root, image=cell.image_tk, text=cell.title, compound=tk.BOTTOM)
             label.grid(row=i, column=0)
             label.bind("<Button-1>", lambda event, cell=cell: self.on_button_clicked(cell))
