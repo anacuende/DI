1bf5e5d68cefa9ffa4cde985b1265175af369d5c
anacuende <113435331+anacuende@users.noreply.github.com>
2023-10-11 12:54:41 +0200

    Comentado yes_window.py

--- sprint1Tkinter/dialog/yes_window.py
+++ sprint1Tkinter/dialog/yes_window.py
@@ -1,14 +1,21 @@
+# Importa la biblioteca tkinter como tk
 import tkinter as tk
 
+# Define una clase llamada YesWindow
 class YesWindow:
     def __init__(self, root):
         self.root = root
+        # Establece el título de la ventana
         self.root.title("Ventana I")
 
+        # Crea una etiqueta en la ventana con el texto "Sí"
         self.label = tk.Label(self.root, text="Sí")
         self.label.pack()
 
 if __name__ == "__main__":
+    # Crea una instancia de la clase Tk para crear la ventana principal
     root = tk.Tk()
+    # Crea una instancia de la clase YesWindow en la ventana principal
     app = YesWindow(root)
+    # Inicia el bucle principal de la aplicación para mantener la ventana abierta
     root.mainloop()
