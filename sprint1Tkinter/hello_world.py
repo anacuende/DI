c8f898b415ee329d59ca173efda8ba263cf2d646
anacuende <113435331+anacuende@users.noreply.github.com>
2023-10-11 12:39:38 +0200

    Comentado hello_world.py

--- sprint1Tkinter/hello_world.py
+++ sprint1Tkinter/hello_world.py
@@ -1,7 +1,11 @@
+# Importa la clase Tk de la biblioteca Tkinter
 from tkinter import Tk
 
+# Crea una instancia de la ventana principal
 root = Tk()
 
+# Establece el título de la ventana principal como "Hola mundo"
 root.title("Hola mundo")
 
+# Inicia el bucle principal de la aplicación para mantener la ventana abierta
 root.mainloop()
