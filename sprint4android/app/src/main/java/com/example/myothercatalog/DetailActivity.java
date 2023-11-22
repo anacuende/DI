cb57c465fa62ca5bca683ce4402c8c03f007b0ca
anacuende <acuendet@fpcoruna.afundacion.org>
2023-11-22 08:50:55 +0100

    Ejercicio 3, añadido renderizado de datos en detail activity

--- sprint4android/app/src/main/java/com/example/myothercatalog/DetailActivity.java
+++ sprint4android/app/src/main/java/com/example/myothercatalog/DetailActivity.java
@@ -2,7 +2,13 @@ package com.example.myothercatalog;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,15 @@ public class DetailActivity extends AppCompatActivity {
         String name = intent.getStringExtra("name");
         String description = intent.getStringExtra("description");
         String imageUrl = intent.getStringExtra("imageUrl");
+
+        // Referenciar los elementos
+        TextView textViewName = findViewById(R.id.textViewTitle);
+        TextView textViewDescription = findViewById(R.id.textViewDescription);
+        ImageView imageViewGame = findViewById(R.id.imageView);
+
+        // Establecer los datos en los elementos
+        textViewName.setText(name);
+        textViewDescription.setText(description);
+        Picasso.get().load(imageUrl).into(imageViewGame);
     }
 }
